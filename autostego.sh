0fae7368b31f2a2533fe2da50e6855791ab92fed
dmcyberkiller <164518476+dmcyberkiller@users.noreply.github.com>
2024-05-23 20:42:56 +0530

    Update autostego.sh

--- autostego.sh
+++ autostego.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-#For Banner
-
 	print_banner() {
     echo "******************************************			*********************"| lolcat
     echo "*    Welcome to My Auto Steghide Tool     *			*                   *"| lolcat
@@ -11,8 +8,6 @@
 }	
 
 print_banner
-
-# Function to hide a file in an image using steghide with password protection
 hide_file() {
     local image=$1
     local file_to_hide=$2
@@ -25,8 +20,6 @@ hide_file() {
         echo "Failed to hide file '$file_to_hide' in '$image'"
     fi
 }
-
-# Function to extract a file from an image using steghide with password protection
 extract_file() {
     local image=$1
     local password=$2
@@ -38,11 +31,6 @@ extract_file() {
         echo "Failed to extract file from '$image'"
     fi
 }
-
-
-
-
-# Check if the correct number of arguments are provided
 if [ "$#" -ne 4 ]; then
 echo "# If Embed, Then Use: $0 <image_file> <file_to_hide> <password> <embed>"
 echo "# If Extract, Then Use: $0 <image_file> <extracted_file_name> <password> <extract>"
@@ -50,18 +38,11 @@ echo "# Also you can read my README.txt file" | lolcat
 exit 1
 fi
 	
-	
-
-
-
-
-# Assign command line arguments to variables
 image=$1
 file_to_hide_extracted_file=$2
 password=$3
 action=$4
 
-# Check if the image and file exist
 if [ ! -f "$image" ]; then
     echo "Error: Image file '$image' not found"
     exit 1
@@ -72,7 +53,6 @@ if [ "$action" == "embed" ] && [ ! -f "$file_to_hide_extracted_file" ]; then
     exit 1
 fi
 
-# Perform the specified action
 case $action in
     embed)
         hide_file "$image" "$file_to_hide_extracted_file" "$password"
